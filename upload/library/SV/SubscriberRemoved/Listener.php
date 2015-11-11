a8239c8ed0f7adb8fe1e5f04a82e3052cab0a2a5
Xon <tazipper1@hotmail.com>
2015-11-12 07:57:16 +0800

    Minor cleanup

--- upload/library/SV/SubscriberRemoved/Listener.php
+++ upload/library/SV/SubscriberRemoved/Listener.php
@@ -2,10 +2,10 @@
 
 class SV_SubscriberRemoved_Listener
 {
-    const AddonNameSpace = 'SV_SubscriberRemoved';
+    const AddonNameSpace = 'SV_SubscriberRemoved_';
 
     public static function load_class($class, array &$extend)
     {
-        $extend[] = self::AddonNameSpace.'_'.$class;
+        $extend[] = self::AddonNameSpace.$class;
     }
 }
